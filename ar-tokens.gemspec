cda99b7ab558d48635102ef62300b2e10257efd1
Paul Spieker <p.spieker@duenos.de>
2014-03-17 11:55:08 +0100

    add coveralls

--- ar-tokens.gemspec
+++ ar-tokens.gemspec
@@ -20,4 +20,6 @@ Gem::Specification.new do |s|
   s.require_paths = ["lib"]
 
   s.add_dependency 'activerecord'
+
+  s.add_development_dependency "coveralls", "~> 0"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,9 @@ require 'bundler/setup'
 require 'ar-tokens'
 require 'support/active_record'
 
+require 'coveralls'
+Coveralls.wear!
+
 RSpec.configure do |config|
   config.around do |example|
     ActiveRecord::Base.transaction do
